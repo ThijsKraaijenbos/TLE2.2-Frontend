be308a5b6574a2501dc908dda2e5112f9cd0ed59
J-Hoogvliet <justinhoogvliet@outlook.com>
2025-06-12 15:43:42 +0200

    Add - midnight check

--- Components/HomeScreen.jsx
+++ Components/HomeScreen.jsx
@@ -127,6 +127,7 @@ export default function HomeScreen({navigation}) {
                         'Accept': 'application/json'
                     }
                 })
+
                 const data = await response.json()
                 setStreak(data)
             } catch (e) {
