fbb2f8ca134eaba7dd14f34ae7ecdb7a79ee270d
J-Hoogvliet <justinhoogvliet@outlook.com>
2025-06-12 15:44:46 +0200

    Add - midnight check

---
+++ Components/ProfileContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState, useContext } from 'react'
+
+const ProfileContext = createContext()
+
+export function ProfileProvider({ children }) {
+    const [profileImage, setProfileImage] = useState(require('../assets/fruitbackground.png'))
+    const [displayName, setDisplayName] = useState("Mand A. Rijn")
+
+    return (
+        <ProfileContext.Provider value={{ profileImage, setProfileImage, displayName, setDisplayName }}>
+            {children}
+        </ProfileContext.Provider>
+    )
+}
+
+export const useProfile = () => useContext(ProfileContext)
