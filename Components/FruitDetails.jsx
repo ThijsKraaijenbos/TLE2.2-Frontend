c896469f98482aab4c4de661b827a821b18976ca
sildatema <sildatema04@gmail.com>
2025-06-24 15:38:32 +0200

    small change

--- Components/FruitDetails.jsx
+++ Components/FruitDetails.jsx
@@ -163,7 +163,7 @@ export default function FruitDetails({ navigation, route }) {
 
                 {/* Fun Fact */}
                 <View style={styles.funFactBox}>
-                    <Text style={styles.funFactText}>fun fact:{"\n"} {Fruitdata.fun_facts}</Text>
+                    <Text style={styles.funFactText}>fun fact:{"\n"} {Fruitdata.fun_facts[0]}</Text>
                 </View>
             </View>
             <BottomNavigation navigation={navigation}/>
